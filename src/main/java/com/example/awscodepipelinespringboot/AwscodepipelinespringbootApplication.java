d191bdb69779fe3db338f62e62b340cb33f1ed6c
EssakiRajan <esakirajan750@gmail.com>
2024-10-08 18:56:14 +0530

    Initial commit

---
+++ src/main/java/com/example/awscodepipelinespringboot/AwscodepipelinespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.awscodepipelinespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwscodepipelinespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwscodepipelinespringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awscodepipelinespringboot/AwscodepipelinespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscodepipelinespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwscodepipelinespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
